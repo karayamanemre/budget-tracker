b9110974828eeb97ab13ee212a61c836ebb141f6
Emre Karayaman <karayaman.emre@outlook.com>
2023-02-13 13:50:13 +0300

    views update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'groups/index', as: :user_root
+
+  root "static_pages#homepage"
+
+  resources :users, :proceedings, :groups
 end
