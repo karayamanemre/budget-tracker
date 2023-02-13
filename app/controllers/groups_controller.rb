d309a8ef0950566727fb10ec25c300dfc0d2841c
Emre Karayaman <karayaman.emre@outlook.com>
2023-02-14 01:28:47 +0300

    style updates

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -25,7 +25,7 @@ class GroupsController < ApplicationController
 
     respond_to do |format|
       if @group.save
-        format.html { redirect_to group_url(@group), notice: 'Group was successfully created.' }
+        format.html { redirect_to group_url(@group), notice: 'Category was successfully created.' }
         format.json { render :show, status: :created, location: @group }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class GroupsController < ApplicationController
   def update
     respond_to do |format|
       if @group.update(group_params)
-        format.html { redirect_to group_url(@group), notice: 'Group was successfully updated.' }
+        format.html { redirect_to group_url(@group), notice: 'Category was successfully updated.' }
         format.json { render :show, status: :ok, location: @group }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +52,7 @@ class GroupsController < ApplicationController
     @group.destroy
 
     respond_to do |format|
-      format.html { redirect_to groups_path, notice: 'Group was successfully destroyed.' }
+      format.html { redirect_to groups_path, notice: 'Category was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
