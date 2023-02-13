08cdc95db7493083467fa066e6c8360e51eabca3
Emre Karayaman <karayaman.emre@outlook.com>
2023-02-13 11:11:00 +0300

    models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :update_signup_params, if: :devise_controller?
+
+  protected
+
+  def update_signup_params
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+  end
 end
