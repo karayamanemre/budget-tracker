861293bd495a6cb255d3023277cce006518a29fd
Emre Karayaman <karayaman.emre@outlook.com>
2023-02-14 19:24:38 +0300

    required changes made

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -25,7 +25,7 @@ class GroupsController < ApplicationController
 
     respond_to do |format|
       if @group.save
-        format.html { redirect_to group_url(@group), notice: 'Category was successfully created.' }
+        format.html { redirect_to groups_path, notice: 'Category was successfully created.' }
         format.json { render :show, status: :created, location: @group }
       else
         format.html { render :new, status: :unprocessable_entity }
