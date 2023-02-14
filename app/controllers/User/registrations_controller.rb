4c93cf6df424f01a3fef0912d5d1a078156948ba
Emre Karayaman <karayaman.emre@outlook.com>
2023-02-14 14:49:53 +0300

    linters

--- app/controllers/User/registrations_controller.rb
+++ app/controllers/User/registrations_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class User::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
@@ -50,7 +48,7 @@ class User::RegistrationsController < Devise::RegistrationsController
   #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
   # end
 
-  def after_sign_up_path_for(resource)
+  def after_sign_up_path_for(_resource)
     groups_path
   end
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :update_signup_params, if: :devise_controller?
 
-  def after_sign_up_path_for(resource)
+  def after_sign_up_path_for(_resource)
     groups_index_path
   end
 
